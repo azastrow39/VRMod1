9cdcf6d9d08502b81206b3d7a1f6b7fcf8154a2d
Matt Lowe <matlow100@yahoo.com>
2021-02-01 16:19:55 -0600

    Added new pacer

---
+++ Assets/Scripts/Pace2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class Pace2 : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float zMax = 7.5f;
+    public float zMin = -7.5f;
+    private int direction = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zNew = transform.position.z + direction * speed * Time.deltaTime;
+        if (zNew >= zMax)
+        {
+            zNew = zMax;
+            direction *= -1;
+        } else if(zNew <= zMin)
+        {
+            zNew = zMin;
+            direction *= -1;
+        }
+        transform.position = new Vector3(-7.5f, 0.75f, zNew);
+    }
+}
